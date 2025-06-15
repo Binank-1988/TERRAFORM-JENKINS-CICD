e744f5552e4a16586dd5a21e43536c4997b0b020
Binank-1988 <binank1988@gmail.com>
2025-06-15 16:46:49 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "jenkin-ci-cd"
+    bucket         = "jenkin-ci-cd15"
     key            = "my-terraform-environment/main"
     region         = "us-east-1"
     dynamodb_table = "jenkins-dynamo-table"
