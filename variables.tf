fe5c3fb71d33c315cc69b92ce3fe4d210652f532
Binank-1988 <binank1988@gmail.com>
2025-06-15 16:44:55 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "ami_id" {
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
   type        = string
-  default     = "examplebucket15062025001"
+  default     = "examplebucket15062025002"
 }
 
 variable "acl" {
